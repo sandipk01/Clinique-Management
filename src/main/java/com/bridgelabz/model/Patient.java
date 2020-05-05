2e1c5ddfd194865402f940014b041a13cd77afdd
sandipk01 <sandipkengar01@gmail.com>
2020-05-05 20:02:37 +0530

    ADDED : Patient model is added

--- src/main/java/com/bridgelabz/model/Patient.java
+++ src/main/java/com/bridgelabz/model/Patient.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.model;
 
-public class Patient {
+public class Patient extends Person {
+
+    private String phone;
+    private long age;
+
+    public Patient(String name, int id, String phone, long age) {
+        super(name, id);
+        this.phone = phone;
+        this.age = age;
+    }
 }
