09cb7981bb508dbfca53ea38b2fee604c6ef75c0
sandipk01 <sandipkengar01@gmail.com>
2020-05-05 20:02:13 +0530

    ADDED : Doctor model is added

--- src/main/java/com/bridgelabz/model/Doctor.java
+++ src/main/java/com/bridgelabz/model/Doctor.java
@@ -1,4 +1,16 @@
 package com.bridgelabz.model;
 
-public class Doctor {
+import com.bridgelabz.enums.Availability;
+
+public class Doctor extends Person {
+
+    private String specialization;
+    private Availability availability;
+
+    public Doctor(String name, int id, String specialization, Availability availability) {
+        super(name, id);
+        this.specialization = specialization;
+        this.availability = availability;
+    }
+
 }
