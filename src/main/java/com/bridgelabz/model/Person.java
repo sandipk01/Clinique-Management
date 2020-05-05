5b8588b71233a8796483ae0dda9d18d158124156
sandipk01 <sandipkengar01@gmail.com>
2020-05-05 20:02:54 +0530

    ADDED : Person model is added

--- src/main/java/com/bridgelabz/model/Person.java
+++ src/main/java/com/bridgelabz/model/Person.java
@@ -1,4 +1,11 @@
 package com.bridgelabz.model;
 
 public class Person {
+    private String name;
+    private int id;
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 }
