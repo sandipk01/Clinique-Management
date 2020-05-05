f61daf25cd5e3a0822b6ecc7e3e2d8e0cee8a7b5
sandipk01 <sandipkengar01@gmail.com>
2020-05-05 20:01:52 +0530

    ADDED : Appointment model is added

--- src/main/java/com/bridgelabz/model/Appointment.java
+++ src/main/java/com/bridgelabz/model/Appointment.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Appointment {
+    private int doctorId;
+    private Date date;
+    private ArrayList<Integer> patientIds = new ArrayList<>();
 }
