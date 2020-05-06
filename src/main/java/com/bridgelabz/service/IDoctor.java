09eef29b004de5ca82796f8c8af68f8ec8e5962e
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 08:51:00 +0530

    ADDED : Add and print doctors method declared

--- src/main/java/com/bridgelabz/service/IDoctor.java
+++ src/main/java/com/bridgelabz/service/IDoctor.java
@@ -1,4 +1,12 @@
 package com.bridgelabz.service;
 
+import com.bridgelabz.model.Doctor;
+
+import java.io.IOException;
+
 public interface IDoctor {
+
+    void addDoctor(Doctor doctor) throws IOException;
+
+    void printDoctor() throws IOException;
 }
