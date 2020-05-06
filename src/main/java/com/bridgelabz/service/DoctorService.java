ae84c821952d8f6ca58c245ba1e2a8b8ab407299
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 08:47:22 +0530

    ADDED : Add doctor and print methods added

--- src/main/java/com/bridgelabz/service/DoctorService.java
+++ src/main/java/com/bridgelabz/service/DoctorService.java
@@ -1,4 +1,31 @@
 package com.bridgelabz.service;
 
-public class Doctor {
+import com.bridgelabz.model.Doctor;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DoctorService extends Manage implements IDoctor {
+
+    private File file = new File("src\\test\\resources\\doctor.json");
+    private List<Doctor> doctorList;
+
+    @Override
+    public void addDoctor(Doctor doctor) throws IOException {
+        if (file.length() == 0)
+            doctorList = new ArrayList<>();
+        else
+            doctorList = FileSystem.readFile(file);
+        FileSystem.saveFile(file, addEntry(doctorList, doctor));
+    }
+
+    @Override
+    public void printDoctor() throws IOException {
+        doctorList = FileSystem.readFile(file);
+        printEntry(doctorList);
+    }
+
+
 }
