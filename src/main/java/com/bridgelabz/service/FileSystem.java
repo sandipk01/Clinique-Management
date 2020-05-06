a27b46f4148909212fd516d3d274a7fa9ac8383a
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 08:48:21 +0530

    ADDED : FileSystem read and save file methods added

--- src/main/java/com/bridgelabz/service/FileSystem.java
+++ src/main/java/com/bridgelabz/service/FileSystem.java
@@ -1,4 +1,31 @@
 package com.bridgelabz.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileSystem {
+
+    private static ObjectMapper mapper;
+
+    private FileSystem() {
+
+    }
+
+    static {
+        mapper = new ObjectMapper();
+    }
+
+    public static <T> void saveFile(File file, List<T> list) throws IOException {
+        mapper.writeValue(file, list);
+    }
+
+    public static <T> List<T> readFile(File file) throws IOException {
+        return mapper.readValue(file, new TypeReference<ArrayList<T>>() {
+        });
+    }
 }
