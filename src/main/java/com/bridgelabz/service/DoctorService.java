386a54e7305b14c587146d98c58e67847549aea6
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 22:07:16 +0530

    REFACTORE : FIle System read file invokation arguments added

--- src/main/java/com/bridgelabz/service/DoctorService.java
+++ src/main/java/com/bridgelabz/service/DoctorService.java
@@ -10,21 +10,30 @@ import java.util.List;
 public class DoctorService extends Manage implements IDoctor {
 
     private File file = new File("src\\test\\resources\\doctor.json");
+
     private List<Doctor> doctorList;
 
     @Override
-    public void addDoctor(Doctor doctor) throws IOException {
-        if (file.length() == 0)
+    public void addDoctor(Doctor doctor) throws IOException, ClassNotFoundException {
+        if (file.length() == 0) {
+            doctor.setId(1);
             doctorList = new ArrayList<>();
-        else
-            doctorList = FileSystem.readFile(file);
+        } else {
+            doctorList = FileSystem.readFile(file, Doctor.class);
+            doctor.setId(doctorList.size() + 1);
+        }
         FileSystem.saveFile(file, addEntry(doctorList, doctor));
     }
 
     @Override
-    public void printDoctor() throws IOException {
-        doctorList = FileSystem.readFile(file);
+    public void printDoctor() throws IOException, ClassNotFoundException {
+        doctorList = FileSystem.readFile(file, Doctor.class);
         printEntry(doctorList);
+    }
+
+    @Override
+    public File getFile() {
+        return file;
     }
 
 
