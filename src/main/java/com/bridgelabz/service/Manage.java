18006e2c87f6cb80baf3f23d40737b79b7dff196
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 08:51:50 +0530

    ADDED : Add and print generic methods are implemented

--- src/main/java/com/bridgelabz/service/Manage.java
+++ src/main/java/com/bridgelabz/service/Manage.java
@@ -1,30 +1,17 @@
 package com.bridgelabz.service;
 
-import com.bridgelabz.model.Doctor;
-import com.bridgelabz.model.Patient;
-
-import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
-public class CliniqueManage {
+public class Manage {
 
-    public static <T> List<T> addEntry(List<T> list, T obj) {
+    public <T> List<T> addEntry(List<T> list, T obj) {
         list.add(obj);
         return list;
     }
 
-    public static <T> void printEntry(List<T> list) {
-        list.forEach(s -> System.out.println());
+    public <T> void printEntry(List<T> list) {
+        list.forEach(s -> System.out.println(s.toString().replace("{", "")
+                .replace("}", "").replace(",","  ")));
     }
 
-    public static <T> List<T> searchEntry(Collection<T> collectionObject,
-                                     Function<T, String> searchPropertyAccessor, String searchText) {
-        List<T> result = collectionObject.stream()
-                .filter(item -> Objects.equals(searchPropertyAccessor.apply(item), searchText))
-                .collect(Collectors.toList());
-        return result;
-    }
 }
