ebd0a498f5a3d56774363dc3f837990f7c25220d
sandipk01 <sandipkengar01@gmail.com>
2020-05-06 08:45:31 +0530

    REFACTORED : Patient model

--- src/main/java/com/bridgelabz/model/Patient.java
+++ src/main/java/com/bridgelabz/model/Patient.java
@@ -1,13 +1,25 @@
 package com.bridgelabz.model;
 
-public class Patient extends Person {
-
+public class Patient {
+    private int id;
+    private String name;
     private String phone;
     private long age;
 
-    public Patient(String name, int id, String phone, long age) {
-        super(name, id);
+    public Patient(int id, String name, String phone, long age) {
+        this.id = id;
+        this.name = name;
         this.phone = phone;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
