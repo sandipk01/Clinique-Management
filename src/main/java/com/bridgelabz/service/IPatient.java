cc945f5e57e6c0aaddcf38b84b44f8f8afb84bc7
sandipk01 <sandipkengar01@gmail.com>
2020-05-07 21:33:08 +0530

    ADDED : Patient search by id method is declared

--- src/main/java/com/bridgelabz/service/IPatient.java
+++ src/main/java/com/bridgelabz/service/IPatient.java
@@ -1,10 +1,10 @@
 package com.bridgelabz.service;
 
-import com.bridgelabz.model.Doctor;
 import com.bridgelabz.model.Patient;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public interface IPatient {
 
@@ -13,4 +13,8 @@ public interface IPatient {
     void printPatient() throws IOException, ClassNotFoundException;
 
     File getFile();
+
+    List<Patient> searchByName(String name) throws IOException, ClassNotFoundException;
+
+    List<Patient> searchByPhone(String phone) throws IOException, ClassNotFoundException;
 }
