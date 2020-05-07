02ef36312c3398af08f109bb51e0812ebc2fd16e
sandipk01 <sandipkengar01@gmail.com>
2020-05-07 12:40:11 +0530

    REFACTORED : Parameter string to object is changed

--- src/main/java/com/bridgelabz/service/Manage.java
+++ src/main/java/com/bridgelabz/service/Manage.java
@@ -18,13 +18,12 @@ public class Manage {
                 .replace("}", "").replace(",", "  ")));
     }
 
-    public static <T> List<T> search(Collection<T> collectionObject,
-                                     Function<T, String> searchPropertyAccessor, String searchText) {
+    public static <T,E> List<T> search(Collection<T> collectionObject,
+                                     Function<T, Object> searchPropertyAccessor, E searchText) {
 
         List<T> result = collectionObject.stream()
                 .filter(item -> Objects.equals(searchPropertyAccessor.apply(item), searchText))
                 .collect(Collectors.toList());
         return result;
     }
-
 }
