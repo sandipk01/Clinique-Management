e2dcfe22175cc8ee73275c45a467e0f4f7341ab1
sandipk01 <sandipkengar01@gmail.com>
2020-05-08 18:17:12 +0530

    ADDED : Setter methods are added

--- src/main/java/com/bridgelabz/model/Appointment.java
+++ src/main/java/com/bridgelabz/model/Appointment.java
@@ -1,11 +1,64 @@
 package com.bridgelabz.model;
 
-import java.util.Date;
-import java.util.List;
+import com.bridgelabz.enums.Availability;
+
 
 public class Appointment {
     private int id;
-    private Date date;
+    private String date;
     private int doctorId;
-    private List<Patient> patients;
+    private int patientId;
+    private Availability availability;
+
+    public Appointment() {
+
+    }
+
+    public Appointment(String date, int doctorId, int patientId, Availability availability) {
+        this.date = date;
+        this.doctorId = doctorId;
+        this.patientId = patientId;
+        this.availability = availability;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public Availability getAvailability() {
+        return availability;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAvailability(Availability availability) {
+        this.availability = availability;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", date=" + date +
+                ", doctorId=" + doctorId +
+                ", patientId=" + patientId +
+                ", Availability= " + availability;
+    }
 }
