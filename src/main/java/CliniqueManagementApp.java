eb1132a660b1fd274574c09a254b88c3789e9d19
sandipk01 <sandipkengar01@gmail.com>
2020-05-08 18:18:42 +0530

    ADDED : Main method is added

--- src/main/java/CliniqueManagementApp.java
+++ src/main/java/CliniqueManagementApp.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import com.bridgelabz.controller.CliniqueManagementController;
+import com.bridgelabz.service.CliniqueService;
+
+import java.io.IOException;
 
 public class CliniqueManagementApp {
+    public static void main(String[] args) {
+        CliniqueManagementController cliniqueManagementController=new CliniqueManagementController();
+    }
 }
