6f01c008b6b25d3e50bf874de8a1185017927061
sandipk01 <sandipkengar01@gmail.com>
2020-05-08 18:19:42 +0530

    ADDED : take appointment and check availability methods are declared

--- src/main/java/com/bridgelabz/service/IClinique.java
+++ src/main/java/com/bridgelabz/service/IClinique.java
@@ -1,4 +1,22 @@
 package com.bridgelabz.service;
 
-public class IClinique {
+import com.bridgelabz.enums.Availability;
+import com.bridgelabz.model.Appointment;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface IClinique {
+
+    boolean takeAppointment(Appointment appointment) throws IOException, ClassNotFoundException;
+
+    File getFile();
+
+    boolean checkDate(String date,int doctorId) throws IOException, ClassNotFoundException;
+
+    boolean checkAvailabilityAtPm(String date, int doctorId) throws IOException, ClassNotFoundException;
+
+    boolean checkAvailabilityAtAm(String date, int doctorId) throws IOException, ClassNotFoundException;
+
+    Availability getDoctorAvailabilityById(int doctorId) throws IOException, ClassNotFoundException;
 }
