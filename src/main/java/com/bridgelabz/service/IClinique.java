d8527c319bc676f72ab4dbd8e97350d3b0eca91a
sandipk01 <sandipkengar01@gmail.com>
2020-05-09 11:01:51 +0530

    ADDED : Most popular doctor and specialization method is declared

--- src/main/java/com/bridgelabz/service/IClinique.java
+++ src/main/java/com/bridgelabz/service/IClinique.java
@@ -5,6 +5,7 @@ import com.bridgelabz.model.Appointment;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public interface IClinique {
 
@@ -12,4 +13,9 @@ public interface IClinique {
 
     File getFile();
 
+    void printReport(int doctorId) throws IOException, ClassNotFoundException;
+
+    int getMostPopularDoctor() throws IOException, ClassNotFoundException;
+
+    String getMostPopularSpecialization()throws IOException, ClassNotFoundException;
 }
