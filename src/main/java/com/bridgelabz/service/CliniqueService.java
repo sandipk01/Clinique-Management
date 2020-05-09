aae5998e79cc8143aed4a03f3bbf60dee48c02fe
sandipk01 <sandipkengar01@gmail.com>
2020-05-09 11:01:37 +0530

    ADDED : Most popular doctor and specialization method is added

--- src/main/java/com/bridgelabz/service/CliniqueService.java
+++ src/main/java/com/bridgelabz/service/CliniqueService.java
@@ -4,14 +4,16 @@ import com.bridgelabz.enums.Availability;
 import com.bridgelabz.exception.CliniqueManagmentException;
 import com.bridgelabz.model.Appointment;
 import com.bridgelabz.model.Doctor;
+import com.bridgelabz.model.Patient;
 import com.bridgelabz.util.FileSystem;
 
 
+import javax.swing.text.html.Option;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CliniqueService extends Manage implements IClinique {
 
@@ -21,13 +23,17 @@ public class CliniqueService extends Manage implements IClinique {
 
     private IDoctor doctorService;
 
+    private IPatient patientService;
+
     public CliniqueService() {
         doctorService = new DoctorService();
+        patientService = new PatientService();
     }
 
     public File getFile() {
         return file;
     }
+
 
     @Override
     public boolean takeAppointment(Appointment appointment) throws IOException, ClassNotFoundException, CliniqueManagmentException {
@@ -77,7 +83,42 @@ public class CliniqueService extends Manage implements IClinique {
                 .filter((doctor1) -> doctor1.getId() == doctorId).findFirst().get();
         return doctor.getAvailability();
     }
+
+    @Override
+    public void printReport(int doctorId) throws IOException, ClassNotFoundException {
+        List<Appointment> reportList = FileSystem.readFile(this.getFile(), Appointment.class);
+        for (Appointment report : reportList)
+            if (report.getDoctorId() == doctorId)
+                System.out.println("Patient Id: " + report.getPatientId() + " " + "Patient Name: " + getPatientNameById(report.getPatientId()));
+    }
+
+    private String getPatientNameById(int patientId) throws IOException, ClassNotFoundException {
+        List<Patient> doctorList = FileSystem.readFile(patientService.getFile(), Patient.class);
+        Patient patient = doctorList.stream()
+                .filter((p) -> p.getId() == patientId).findFirst().get();
+        return patient.getName();
+    }
+
+    @Override
+    public int getMostPopularDoctor() throws IOException, ClassNotFoundException {
+        List<Appointment> doctorList = FileSystem.readFile(this.getFile(), Appointment.class);
+        return doctorList.stream()
+                .collect(Collectors.groupingBy(s -> s.getDoctorId(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .map(s -> s.getKey().intValue()).get();
+    }
+
+    @Override
+    public String getMostPopularSpecialization() throws IOException, ClassNotFoundException {
+        return getSpecializationByDoctorId(getMostPopularDoctor());
+    }
+
+    private String getSpecializationByDoctorId(int doctorId) throws IOException, ClassNotFoundException {
+        List<Doctor> doctorList = FileSystem.readFile(doctorService.getFile(), Doctor.class);
+        Doctor doctor = doctorList.stream()
+                .filter((doctor1) -> doctor1.getId() == doctorId).findFirst().get();
+        return doctor.getSpecialization();
+    }
 }
-
-
-
